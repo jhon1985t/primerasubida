f4e004245a62fbeaba7f600c475a46195cc11e01
Jhon Tabares <jhon1985t@gmail.com>
2019-03-27 09:19:06 -0500

    entrega

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const {obtenerPromedio, argv} = require('./datos.js')
 
-const express = require('express')
-const app = express()
-
 console.log(argv);
 if (argv._[0]=='promedio'){
-    texto = ('El promedio de ' + argv.n + ' es ' + obtenerPromedio);
+    console.log('El promedio de ' + argv.n + ' es ' + obtenerPromedio(argv.m,argv.i,argv.p));
 } 
 else {
 	console.log (' promedio no calculado ');
 } 
-
-app.get('/', function(req, res) {
-    res.send(texto)
-})
-
-app.listen(3000)
